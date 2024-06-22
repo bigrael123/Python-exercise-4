4205d703ea0f075648b436c4d75c7b71bbae6861
bigrael123 <bigrael-x@hotmail.com>
2024-06-21 23:06:50 -0300

    prints added

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 s1 = "ant" #attribute the string ant to s1
 s2 = "bat" #attribute the string ant to s2
 s3 = "cod" #attribute the string ant to s3
-print( s1+ " " + s2+" " + s3)
+print( s1+ " " + s2+" " + s3) #write "ant bat cod"
+print((s1 +" ") *10) #write ant 10 times
+print(s1 + " " + (s2+" ")*2 + (s3+" ") * 3)  #write ant bat bat cod cod cod
+print(s1 + " " + s2 + " " + s1 + " " + s2 + " "+ s1 + " "+ s2+ " " + s1 + " " + s2 + " " + s1 + " " + s2 + " " + s1 + " " + s2 + " " + s1 + " " + s2) #write ant bant 7 times
+print((((s2*2)+s3+" ")*5)) #write batbatcod 5 times
